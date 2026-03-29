229905295aa85812f05ad3eb376cc432427bfc31
Dheftyr Marcon <dheftyr_fullstack@hotmail.com>
2026-03-28 22:24:03 -0300

    Criando o exercício 3

---
+++ dist/exercicios/exercicio3.js
@@ -0,0 +1,42 @@
+let number = Number(prompt("Digite a opção: \n1 - Soma \n2 - Subtração \n3 - Divisão \n4 - Multiplicação"));
+switch (number) {
+    case 1:
+        let soma = 0;
+        let numberS1 = Number(prompt("Digite o primeiro número:"));
+        let numberS2 = Number(prompt("Digite o segundo número:"));
+        soma = numberS1 + numberS2;
+        ;
+        alert(`Resultado da soma: ${soma}`);
+        break;
+    case 2:
+        let subtração = 0;
+        let numberSb1 = Number(prompt("Digite o primeiro número:"));
+        let numberSb2 = Number(prompt("Digite o seugundo número:"));
+        subtração = numberSb1 - numberSb2;
+        alert(`Resultado da subtração: ${subtração}`);
+        break;
+    case 3:
+        let divisao = 0;
+        let numberD1 = Number(prompt("Digite o primeiro número:"));
+        let numberD2 = Number(prompt("Digite o seugundo número:"));
+        if (numberD1 === 0) {
+            alert(`O divisor não pode ser ${numberD1}`);
+        }
+        else {
+            divisao = numberD1 / numberD2;
+            alert(`Resultado da divisão: ${divisao}`);
+        }
+        break;
+    case 4:
+        let multiplicacao = 0;
+        let numberM1 = Number(prompt("Digite o primeiro número:"));
+        let numberM2 = Number(prompt("Digite o seugundo número:"));
+        multiplicacao = numberM1 * numberM2;
+        alert(`Resultado da multiplicação ${multiplicacao}`);
+        break;
+    default:
+        alert(`Número não correspondente a opção!`);
+        break;
+}
+export {};
+//# sourceMappingURL=exercicio3.js.map
