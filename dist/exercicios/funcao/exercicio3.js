bd990d29e259995b913bbdf2bf1fb08dc24bbc8e
Dheftyr Dev <dheftyr_fulstack@hotmail.com>
2026-04-19 14:27:24 -0300

    Criando o exercício 3 de função

---
+++ dist/exercicios/funcao/exercicio3.js
@@ -0,0 +1,22 @@
+// 3. Crie uma função que recebe um número (ex: 5) e em seguida utilize loops aninhados para
+// desenhar um triângulo de asteriscos no console.
+// Exemplo para entrada 3:
+// *
+// **
+// ***
+let triangle = ``;
+function desenhar_triangulo(num) {
+    let contLine = 0;
+    for (let i = 1; i <= num; i++) {
+        contLine++;
+        for (let i = 0; i < contLine; i++) {
+            triangle += `*`;
+        }
+        triangle += `\n`;
+    }
+}
+let number = Number(prompt("Digite um número: "));
+desenhar_triangulo(number);
+alert(triangle);
+export {};
+//# sourceMappingURL=exercicio3.js.map
---
+++ src/exercicios/funcao/exercicio3.ts
@@ -0,0 +1,31 @@
+// 3. Crie uma função que recebe um número (ex: 5) e em seguida utilize loops aninhados para
+// desenhar um triângulo de asteriscos no console.
+
+// Exemplo para entrada 3:
+// *
+// **
+// ***
+
+let triangle: string = ``;
+
+function desenhar_triangulo(num:number) {
+    let contLine: number = 0;
+    
+
+    for(let i = 1; i <= num; i++){
+        contLine++;
+        
+        for(let i = 0; i < contLine; i++){
+            triangle += `*`
+        }
+
+        triangle += `\n`
+    }
+
+}
+
+let number: number = Number(prompt("Digite um número: "));
+
+desenhar_triangulo(number);
+
+alert(triangle);
